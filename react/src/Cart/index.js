d7072e6915de318e2440d18d14f214e6b5b768a5
Gustav Svendsen <gsvendsen99@gmail.com>
2019-03-28 09:28:15 +0100

    Update key in array map

--- react/src/Cart/index.js
+++ react/src/Cart/index.js
@@ -7,12 +7,13 @@ import ProductListItem from '../ProductListItem/'
 const Cart = props => {
 
   const cartData = props.data;
+  console.log(cartData)
   return (
     <div className="cart-container">
       <button onClick={props.clearCart}>Clear Cart</button>
       <button onClick={props.toggleCheckout}>Checkout</button>
       <h4>Cart</h4>
-      {cartData.map(item => <ProductListItem data={item} /> )}
+      {cartData.map((item, key) => <ProductListItem key={key} data={item.product_id} /> )}
     </div>
   )
 
