5aee8a5d7b3998a0abe49f34697c0e9ecc5f5f0e
Gustav Svendsen <gsvendsen99@gmail.com>
2019-03-28 09:29:08 +0100

    Update formatting

--- react/src/Product/index.js
+++ react/src/Product/index.js
@@ -5,7 +5,7 @@ import './Product.css'
 const Product = props => {
 
   const productData = props.data;
-  
+
   return (
     <div className="product-container">
       <img className="product-image" src={productData.image} alt={productData.description}></img>
@@ -13,7 +13,6 @@ const Product = props => {
         <h3>{productData.name}</h3>
         <p>{productData.description}</p>
         <i>$ {productData.price}</i>
-        <p>{productData.inventory} left in stock.</p>
         <button onClick={(e) => props.addToCart(e, productData)}>Add To Cart</button>
       </div>
     </div>
