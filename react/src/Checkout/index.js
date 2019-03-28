542af0fa86fb81228a28f6a215e284d9603feb87
Gustav Svendsen <gsvendsen99@gmail.com>
2019-03-28 09:28:54 +0100

    Update checkout style

--- react/src/Checkout/index.js
+++ react/src/Checkout/index.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ProductListItem from '../ProductListItem/'
 
 
 import './Checkout.css'
 
-class Checkout extends Component {
-render () {
+const Checkout = (props) => {
+
+  let totalPrice = 0
+  props.cart.forEach(product => {
+    totalPrice += product.price
+  })
+
   return (
     <div className="checkout-container">
-    <img className="exit-icon" onClick={this.props.toggleCheckout} height="25" width="25" src="https://image.flaticon.com/icons/svg/109/109602.svg"/>
+    <img alt="Exit button for checkout" className="exit-icon" onClick={props.toggleCheckout} height="25" width="25" src="https://image.flaticon.com/icons/svg/109/109602.svg"/>
     <h1>CHECKOUT!!!</h1>
-    {this.props.cart.map(item => <ProductListItem data={item} /> )}
+    <div className="checkout-list">
+      {props.cart.map((item, key) => <ProductListItem key={key} data={item.product_id} /> )}
+    </div>
+    <p>
+      Total Price: $ <i>{totalPrice}</i>
+    </p>
     </div>
   )
-}
+
 }
 
 export default Checkout
