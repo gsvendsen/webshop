ed0201b14a7d94386dc37680f70515088a23238b
Gustav Svendsen <gsvendsen99@gmail.com>
2019-03-28 09:25:59 +0100

    Add services for cart items and orders

--- webshop-api/webshop-api/Services/CartItemsService.cs
+++ webshop-api/webshop-api/Services/CartItemsService.cs
@@ -1,10 +1,67 @@
 ﻿using System;
-namespace webshopapi.Services
+using System.Collections.Generic;
+using webshop_api.Models;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Linq;
+using System.Transactions;
+using webshop_api.Repositories;
+
+namespace webshop_api.Services
 {
     public class CartItemsService
     {
-        public CartItemsService()
+        private readonly ICartItemsRepository cartItemsRepository;
+
+        public CartItemsService(ICartItemsRepository cartItemsRepository)
         {
+            this.cartItemsRepository = cartItemsRepository;
+        }
+
+        public List<CartItem> Get(int id)
+        {
+
+            var cartItems = this.cartItemsRepository.Get(id);
+
+            if (cartItems == null)
+            {
+                return null;
+            }
+
+            return cartItems;
+
+        }
+
+        public bool Add(CartItem cartItem)
+        {
+            if (cartItem == null)
+            {
+                return false;
+            }
+
+            this.cartItemsRepository.Add(cartItem);
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+
+            using (TransactionScope scope = new TransactionScope())
+            {
+                var result = this.cartItemsRepository.Get(id);
+
+                if (result == null)
+                {
+                    return false;
+                }
+
+                this.cartItemsRepository.Delete(id);
+
+                scope.Complete();
+            }
+
+            return true;
+
         }
     }
 }
--- webshop-api/webshop-api/Services/OrdersService.cs
+++ webshop-api/webshop-api/Services/OrdersService.cs
@@ -1,10 +1,47 @@
 ﻿using System;
-namespace webshopapi.Services
+using System.Collections.Generic;
+using webshop_api.Models;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Linq;
+using System.Transactions;
+using webshop_api.Repositories;
+
+namespace webshop_api.Services
 {
     public class OrdersService
     {
-        public OrdersService()
+        private readonly IOrdersRepository ordersRepository;
+
+        public OrdersService(IOrdersRepository ordersRepository)
         {
+            this.ordersRepository = ordersRepository;
         }
+
+        public Order Get(int id)
+        {
+
+            var orderItem = this.ordersRepository.Get(id);
+
+            if (orderItem == null)
+            {
+                return null;
+            }
+
+            return orderItem;
+
+        }
+
+        public bool Add(Order order)
+        {
+            if (order == null)
+            {
+                return false;
+            }
+
+            this.ordersRepository.Add(order);
+            return true;
+        }
+
     }
 }
