46b2e1fd22cba9b28f87d9e8f0e7b72b4545d8d5
Gustav Svendsen <gsvendsen99@gmail.com>
2019-03-28 09:24:23 +0100

    Add Orders controller

--- webshop-api/webshop-api/Controllers/OrdersController.cs
+++ webshop-api/webshop-api/Controllers/OrdersController.cs
@@ -1,10 +1,68 @@
 ﻿using System;
-namespace webshopapi.Controllers
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webshop_api.Models;
+using Newtonsoft.Json;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using webshop_api.Services;
+using webshop_api.Repositories;
+
+namespace webshop_api.Controllers
 {
-    public class OrdersController
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
     {
-        public OrdersController()
+
+        private readonly string connectionString;
+        private readonly OrdersService ordersService;
+
+        public OrdersController(IConfiguration configuration)
         {
+            this.connectionString = configuration.GetConnectionString("ConnectionString");
+            this.ordersService = new OrdersService(new OrdersRepository(connectionString));
+
         }
+
+        // GET api/cartitems/2
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(List<CartItem>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult Get(int id)
+        {
+            var order = ordersService.Get(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
+
+        // POST api/products/
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult Post([FromBody]Order order)
+        {
+            var result = this.ordersService.Add(order);
+
+            if (!result)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
     }
 }
