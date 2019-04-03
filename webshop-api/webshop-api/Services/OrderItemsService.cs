27bec171d1db1c27b52940c0dbd50c0168f3d30a
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-03 18:54:41 +0200

    Update services

--- webshop-api/webshop-api/Services/OrderItemsService.cs
+++ webshop-api/webshop-api/Services/OrderItemsService.cs
@@ -1,10 +1,46 @@
 ﻿using System;
-namespace webshopapi.Services
+using System.Collections.Generic;
+using webshop_api.Models;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Linq;
+using System.Transactions;
+using webshop_api.Repositories;
+
+namespace webshop_api.Services
 {
     public class OrderItemsService
     {
-        public OrderItemsService()
+        private readonly IOrderItemsRepository orderItemsRepository;
+
+        public OrderItemsService(IOrderItemsRepository orderItemsRepository)
         {
+            this.orderItemsRepository = orderItemsRepository;
+        }
+
+        public List<OrderItem> Get()
+        {
+
+            var orderItems = this.orderItemsRepository.Get();
+
+            if (orderItems == null)
+            {
+                return null;
+            }
+
+            return orderItems;
+
+        }
+
+        public bool Add(OrderItem orderItem)
+        {
+            if (orderItem == null)
+            {
+                return false;
+            }
+
+            this.orderItemsRepository.Add(orderItem);
+            return true;
         }
     }
 }
