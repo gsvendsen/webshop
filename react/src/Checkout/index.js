c6a13420af73a9f1e46a8264c9b77db4cca73f3b
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-03 18:50:52 +0200

    Update totalPrice source

--- react/src/Checkout/index.js
+++ react/src/Checkout/index.js
@@ -7,15 +7,6 @@ import './Checkout.css'
 
 const Checkout = (props) => {
 
-  let totalPrice = 0
-  props.cart.forEach(item => {
-    fetch(`http://localhost:5000/api/products/${item.product_id}`)
-      .then(response => response.json())
-      .then(response => {
-        response.price += totalPrice
-      })
-  })
-
   return (
     <div className="checkout-container">
       <img alt="Exit button for checkout" className="exit-icon" onClick={props.toggleCheckout} height="25" width="25" src="https://image.flaticon.com/icons/svg/109/109602.svg"/>
@@ -24,7 +15,7 @@ const Checkout = (props) => {
         {props.cart.map((item, key) => <ProductListItem key={key} data={item.product_id} /> )}
       </div>
       <p>
-        Total Price: $ <i>{totalPrice}</i>
+        Total Price: $ <i>{props.totalPrice}</i>
       </p>
       <ContactForm toggleCart={props.toggleCart} toggleCheckout={props.toggleCheckout} clearCart={props.clearCart} cartData={props.cart} submitOrder={props.submitOrder} />
     </div>
