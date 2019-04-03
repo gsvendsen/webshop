e826fc900d9efb23f336234f9a2d2a30934564d1
Gustav Svendsen <gsvendsen99@gmail.com>
2019-04-03 18:54:29 +0200

    Add service for submitting orders

--- webshop-api/webshop-api/Services/OrdersService.cs
+++ webshop-api/webshop-api/Services/OrdersService.cs
@@ -12,10 +12,17 @@ namespace webshop_api.Services
     public class OrdersService
     {
         private readonly IOrdersRepository ordersRepository;
+        private readonly IOrderItemsRepository orderItemsRepository;
+        private readonly IProductsRepository productsRepository;
+        private readonly ICartItemsRepository cartItemsRepository;
 
-        public OrdersService(IOrdersRepository ordersRepository)
+
+        public OrdersService(IOrdersRepository ordersRepository, IOrderItemsRepository orderItemsRepository, IProductsRepository productsRepository, ICartItemsRepository cartItemsRepository)
         {
             this.ordersRepository = ordersRepository;
+            this.orderItemsRepository = orderItemsRepository;
+            this.productsRepository = productsRepository;
+            this.cartItemsRepository = cartItemsRepository;
         }
 
         public Order Get(int id)
@@ -32,15 +39,36 @@ namespace webshop_api.Services
 
         }
 
-        public bool Add(Order order)
+        // api/order/2   Makes order with Order info from Body and using cart 2
+        public int Add(int cart_id, Order order)
         {
-            if (order == null)
-            {
-                return false;
-            }
+            var orderId = this.ordersRepository.Add(order);
 
-            this.ordersRepository.Add(order);
-            return true;
+            var cartItems = this.cartItemsRepository.Get(cart_id);
+
+            var orderItems = cartItems.Select(cartItem =>
+            {
+                var productItem = this.productsRepository.Get(cartItem.Product_id);
+
+                return new OrderItem
+                {
+                    Order_id = orderId,
+                    Product_name = productItem.Name,
+                    Product_description = productItem.Description,
+                    Product_price = productItem.Price,
+                    Product_quantity = 1
+                };
+
+            }).ToList();
+
+            orderItems.ForEach(orderItem =>
+            {
+                this.orderItemsRepository.Add(orderItem);
+            });
+
+            this.cartItemsRepository.Delete(cart_id);
+
+            return orderId;
         }
 
     }
